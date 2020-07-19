e54cf1dde8714cece24da596ccf4a759fdbeef0f
Cristian <1154@holbertonschool.com>
2020-07-19 15:33:13 +0000

    Adding condition key > length boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,18 +9,25 @@ def canUnlockAll(boxes):
     map_list[0] = True
     copy_keys = []
 
+    if key[0] > len(boxes) - 1:
+        return False
+
     while key:
+        if key[0] > len(boxes) - 1:
+            return False
         if isinstance(boxes[key[0]], list):
             if boxes[key[0]] == []:
                 map_list[key[0]] = True
 
             for i in boxes[key[0]]:
+                if i > len(boxes) - 1:
+                    return False
                 if i not in key and i not in copy_keys:
                     key.append(i)
                     map_list[key[0]] = True
                 elif i in key:
                     map_list[key[0]] = True
                 map_list[key[0]] = True
-        copy = key.pop(0)
-        copy_keys.append(copy)
+            copy = key.pop(0)
+            copy_keys.append(copy)
     return all(map_list)
