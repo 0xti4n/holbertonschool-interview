ab39015993f956e3973ec06b55b4d902e0f999c2
Cristian <1154@holbertonschool.com>
2020-07-19 14:52:12 +0000

    Adding task 0 unlock boxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""unlock boxes"""
+
+
+def canUnlockAll(boxes):
+    """Function that unlock boxes"""
+    key = boxes[0]
+    map_list = [[]] * len(boxes)
+    map_list[0] = True
+    copy_keys = []
+
+    while key:
+        if isinstance(boxes[key[0]], list):
+            if boxes[key[0]] == []:
+                map_list[key[0]] = True
+
+            for i in boxes[key[0]]:
+                if i not in key and i not in copy_keys:
+                    key.append(i)
+                    map_list[key[0]] = True
+                elif i in key:
+                    map_list[key[0]] = True
+                map_list[key[0]] = True
+        copy = key.pop(0)
+        copy_keys.append(copy)
+    return all(map_list)
