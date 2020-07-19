6bb97c6e7b637ee3f8198cf575dbd40bb6292f10
Cristian <1154@holbertonschool.com>
2020-07-19 22:34:37 +0000

    Updating task

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,17 +7,21 @@ def canUnlockAll(boxes):
     keys = [0]
     map_list = list(range(len(boxes)))
     unlock = False
-
+    
     while keys:
         copy_keys = []
         if keys[0] in map_list:
             unlock = True
             for idx in boxes[keys[0]]:
-                if idx not in n_k and idx in map_list:
+                if idx not in keys and idx in map_list:
                     copy_keys.append(idx)
-            map_list.remove(idx)
+            map_list.remove(keys[0])
             keys.pop(0)
+        keys.extend(copy_keys)
         if not unlock:
             return False
         unlock = False
+    
+    if len(map_list) > 0:
+        return False
     return True
