388d1fc008e32028a274c8f3121b48efd10d5b65
Cristian <1154@holbertonschool.com>
2020-08-08 18:07:47 +0000

    Updating task

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,33 +2,22 @@
 """Minimum operations"""
 
 
-def copy_all(H, con):
-    """Copy all"""
-    copy = H
-    con += 1
-    return con, copy
-
-
-def paste(copy, H, con):
-    """Concat copy"""
-    paste = copy + H
-    con += 1
-    return con, paste
-
-
 def minOperations(n):
     """Function that calculates
     minimum operations"""
-    H = 'H'
+    H = 1
+    copy_all = 0
     con = 0
     if type(n) is not int or n < 2:
         return 0
 
     for i in range(n):
-        if len(H) == n:
+        if H == n:
             return con
-        if n % len(H) == 0:
-            con1, copy = copy_all(H, con)
-            con, H = paste(copy, H, con1)
+        if n % H == 0:
+            copy_all = H
+            H += copy_all
+            con += 2
         else:
-            con, H = paste(copy, H, con)
+            H += copy_all
+            con += 1
