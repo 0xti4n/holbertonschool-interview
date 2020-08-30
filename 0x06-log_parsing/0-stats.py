3156cd1918bf54ebeb71e01c36e2b4a1bccce706
Cristian <1154@holbertonschool.com>
2020-08-30 21:28:47 +0000

    Updating task

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python3
-import signal
+"""Log file"""
 import sys
 
 
-def signal_handler(sig, frame):
+status = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0,
+}
+file_size = 0
+con = 1
+
+try:
+    for line in sys.stdin:
+        token = line.split(' ')
+        status_code = token[7]
+        file_s = token[8]
+
+        if status_code in status:
+            status[status_code] += 1
+
+        file_size += int(file_s)
+        if con % 10 == 0:
+            print('File size: {}'.format(file_size))
+            for k, v in sorted(status.items()):
+                if v != 0:
+                    print('{}: {}'.format(k, v))
+        con += 1
+except Exception:
+    pass
+
+finally:
     print('File size: {}'.format(file_size))
     for k, v in sorted(status.items()):
-        print('{}: {}'.format(k, v))
-    sys.exit(0)
-
-status = {}
-file_size = 0
-con = 0
-
-for line in sys.stdin:
-    token = line.split(' ')
-    status_code = token[7]
-    file_s = token[8]
-
-    if status_code in status:
-        status[status_code] += 1
-    else:
-        status[status_code] = 1
-
-    file_size += int(file_s)
-    if con % 10 == 0 and con != 0:
-        print('File size: {}'.format(file_size))
-        for k, v in sorted(status.items()):
+        if v != 0:
             print('{}: {}'.format(k, v))
-    con += 1
-    signal.signal(signal.SIGINT, signal_handler)
