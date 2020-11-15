2b2bb3bf5c8d30ff1473e42e13b2f993283b73e1
Cristian <1154@holbertonschool.com>
2020-11-15 20:51:21 +0000

    Updating task:

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,19 +5,23 @@ const request = require('request');
 
 const url = `https://swapi-api.hbtn.io/api/films/${args[0]}`;
 
-request.get(url, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode === 200) {
-    const data = JSON.parse(response.body);
-    data.characters.forEach(element => {
-      request.get(element, (error, response) => {
-        if (error) console.log(error);
-        else if (response.statusCode === 200) {
-          const dataCharacter = JSON.parse(response.body);
-          console.log(dataCharacter.name);
-        }
-      });
-    });
+request.get(url, async (error, response) => {
+  if (error) console.log(error);
+  else if (response.statusCode === 200) {
+    const data = JSON.parse(response.body).characters;
+    for (let i = 0; i < data.length; i++) {
+      const res = await getCharacter(data[i]);
+      console.log(res);
+    }
   }
 });
+
+function getCharacter (url) {
+  const name = new Promise((resolve) => {
+    request.get(url, (error, response) => {
+      if (error) console.log(error);
+      resolve(JSON.parse(response.body).name);
+    });
+  });
+  return name;
+}
